5f51053364b97895bf415fc098d4de4512692a39
eblack <eblack.leaf@proton.me>
2023-11-25 22:37:38 -0500

    android-build-tool

--- README.md
+++ README.md
@@ -20,16 +20,53 @@ installing packages, here are the
 Follow steps 1 - 4.
 
 You will need to install these tools to your SDK.
-`sdkmanager "platform-tools" "platforms;android-<api-version>"
-"build-tools;<version>" "ndk;<ndk-version>"`.
 
-To run on android, `run --package build_android -- entry <arch>
-<ndk-path> <sdk-path>`
-where `<arch>` can be `arm64-v8a` / `aarch64-linux-android` for ARM support or
+    sdkmanager "platform-tools" "platforms;android-<api-version>" 
+    "build-tools;<version>" "ndk;<ndk-version>"
+
+To run on android,
+
+    run --package build_android -- <path-to-android.toml>
+
+which points to a `.toml` file that describes the environment for compiling to android.
+
+    package = "<package-name>"
+    arch = "<arch>"
+    ndk_home = "/path/to/android_sdk/ndk/<ndk-version>"
+    sdk_home = "/path/to/android_sdk/"
+    min_sdk = <min-api-level>
+    target_sdk = <target-api-level>
+    compile_sdk = <compile-api-level>
+    android_application_version = "<version>"
+    gradle_distribution_url = <distribution-url-id> e.g. "8.0-all"
+    ndk_version = "<ndk-version>"
+    androidx_version = "<version>"
+    androidx_constraintlayout_version = "<version>"
+    androidx_games_activity_version = "<version>"
+    androidx_fragment_version = "<version>"
+    oboe_version = "<version>"
+
+`<arch>` can be `arm64-v8a` / `aarch64-linux-android`
+for ARM support or
 `x86_64-linux-android` for
-x86 support. Example `<ndk-path>` could be
-`/home/<user>/android_sdk/ndk/<version-num>` and the `<sdk-path>`
-would point to the sdk root directory `/home/<user>/android_sdk/`.
+x86 support.
+
+You will need to configure your entry point with `foliage::AndroidApp`
+as a parameter and `#[no_mangle]`
+attribute.
+
+```rust 
+// android app hook
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: foliage::AndroidApp) {
+    // call your regular main here
+}
+```
+
+This will need to be compiled into a `cdylib` to generate a `.so` file for use
+with the `jni` interface.
+`crate-type = ["cdylib"]`
 
 ### Desktop
 
--- build_android/src/main.rs
+++ build_android/src/main.rs
@@ -1,9 +1,11 @@
 use std::ffi::OsString;
 use std::path::{Path, PathBuf};
 
+use serde::Deserialize;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let args = prepare_args(args);
+    let args = prepare_from_file(args);
     build_template(&args);
     build_android(args);
 }
@@ -16,62 +18,111 @@ struct Args {
     working_directory: PathBuf,
     jni_output: PathBuf,
     app_source: PathBuf,
+    min_sdk: String,
+    target_sdk: String,
+    compile_sdk: String,
+    android_application_version: String,
+    gradle_distribution_url: String,
+    ndk_version: String,
+    androidx_version: String,
+    androidx_constraintlayout_version: String,
+    androidx_games_activity_version: String,
+    androidx_fragment_version: String,
+    oboe_version: String,
 }
 
-fn prepare_args(args: Vec<String>) -> Args {
-    if let Some(package) = args.get(1) {
-        if let Some(arch) = args.get(2) {
-            if let Some(ndk_home) = args.get(3) {
-                if let Some(sdk_home) = args.get(4) {
-                    let root = Path::new(env!("CARGO_MANIFEST_DIR"))
-                        .canonicalize()
-                        .unwrap();
-                    let app_source = root.join("app_src");
-                    let jni_output = app_source
-                        .join("app")
-                        .join("src")
-                        .join("main")
-                        .join("jniLibs");
-                    let ndk_home = Path::new(ndk_home).canonicalize().unwrap();
-                    let sdk_home = Path::new(sdk_home).canonicalize().unwrap();
-                    return Args {
-                        package: OsString::from(package),
-                        arch: OsString::from(arch),
-                        ndk_home,
-                        sdk_home,
-                        working_directory: root,
-                        jni_output,
-                        app_source,
-                    };
-                } else {
-                    panic!("could not prepare args: check sdk_home");
-                }
-            } else {
-                panic!("could not prepare args: check ndk_home");
-            }
-        } else {
-            panic!("could not prepare args: check arch");
+#[derive(Deserialize)]
+struct InputArgs {
+    package: String,
+    arch: String,
+    ndk_home: String,
+    sdk_home: String,
+    min_sdk: u32,
+    target_sdk: u32,
+    compile_sdk: u32,
+    android_application_version: String,
+    gradle_distribution_url: String,
+    ndk_version: String,
+    androidx_version: String,
+    androidx_constraintlayout_version: String,
+    androidx_games_activity_version: String,
+    androidx_fragment_version: String,
+    oboe_version: String,
+}
+
+fn prepare_from_file(args: Vec<String>) -> Args {
+    if let Some(filename) = args.get(1) {
+        let absolute_filename = Path::new(filename).canonicalize().unwrap();
+        if let Ok(file) = std::fs::read_to_string(absolute_filename) {
+            let input: InputArgs = toml::from_str(file.as_str()).unwrap();
+            let root = Path::new(env!("CARGO_MANIFEST_DIR"))
+                .canonicalize()
+                .unwrap();
+            let app_source = root.join("app_src");
+            let jni_output = app_source
+                .join("app")
+                .join("src")
+                .join("main")
+                .join("jniLibs");
+            return Args {
+                package: OsString::from(input.package),
+                arch: OsString::from(input.arch),
+                ndk_home: Path::new(input.ndk_home.as_str()).canonicalize().unwrap(),
+                sdk_home: Path::new(input.sdk_home.as_str()).canonicalize().unwrap(),
+                working_directory: root,
+                jni_output,
+                app_source,
+                min_sdk: input.min_sdk.to_string(),
+                target_sdk: input.target_sdk.to_string(),
+                compile_sdk: input.compile_sdk.to_string(),
+                android_application_version: input.android_application_version,
+                gradle_distribution_url: input.gradle_distribution_url,
+                ndk_version: input.ndk_version,
+                androidx_version: input.androidx_version,
+                androidx_constraintlayout_version: input.androidx_constraintlayout_version,
+                androidx_games_activity_version: input.androidx_games_activity_version,
+                androidx_fragment_version: input.androidx_fragment_version,
+                oboe_version: input.oboe_version,
+            };
         }
-    } else {
-        panic!("could not prepare args: check package");
     }
+    panic!("could not prepare args");
 }
 
 fn build_template(args: &Args) {
-    let manifest = std::fs::read_to_string(
-        args.working_directory
-            .join("template")
-            .join("AndroidManifest.xml"),
-    )
+    let manifest_template = args
+        .working_directory
+        .join("template")
+        .join("AndroidManifest.xml");
+    let manifest = std::fs::read_to_string(manifest_template)
         .unwrap()
         .replace("{{package-name}}", args.package.to_str().unwrap())
         .replace("{{package-label}}", "foliage");
-    // let build = std::fs::read_to_string(args.working_directory.join("template").join("build.gradle")).unwrap();
-    let activity = std::fs::read_to_string(
-        args.working_directory
-            .join("template")
-            .join("MainActivity.java"),
-    )
+    let build_template = args.working_directory.join("template").join("build.gradle");
+    let build = std::fs::read_to_string(build_template)
+        .unwrap()
+        .replace("{{compile-sdk}}", args.compile_sdk.as_str())
+        .replace("{{min-sdk}}", args.min_sdk.as_str())
+        .replace("{{target-sdk}}", args.target_sdk.as_str())
+        .replace("{{androidx-version}}", args.androidx_version.as_str())
+        .replace(
+            "{{androidx-constraintlayout-version}}",
+            args.androidx_constraintlayout_version.as_str(),
+        )
+        .replace(
+            "{{androidx-fragment-version}}",
+            args.androidx_fragment_version.as_str(),
+        )
+        .replace("{{oboe-version}}", args.oboe_version.as_str())
+        .replace(
+            "{{androidx-games-activity-version}}",
+            args.androidx_games_activity_version.as_str(),
+        );
+    let activity_template = args
+        .working_directory
+        .join("template")
+        .join("MainActivity.java");
+    let activity = std::fs::read_to_string(activity_template)
         .unwrap()
         .replace("{{package-name}}", args.package.to_str().unwrap());
     let activity_dest = args
@@ -84,7 +135,7 @@ fn build_template(args: &Args) {
         .join("foliage")
         .join("app")
         .join("MainActivity.java");
-    // let build_dest = args.app_source.join("app").join("src").join("build.gradle");
+    let build_dest = args.app_source.join("app").join("build.gradle");
     let manifest_dest = args
         .app_source
         .join("app")
@@ -92,7 +143,7 @@ fn build_template(args: &Args) {
         .join("main")
         .join("AndroidManifest.xml");
     std::fs::write(activity_dest, activity).unwrap();
-    // std::fs::write(build_dest, build).unwrap();
+    std::fs::write(build_dest, build).unwrap();
     std::fs::write(manifest_dest, manifest).unwrap();
 }
 
