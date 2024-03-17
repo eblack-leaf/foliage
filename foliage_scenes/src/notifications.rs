71b441fbc52e469c111c04f86fa2a090a43587ac
eblack <eblack.leaf@proton.me>
2024-03-16 21:01:27 -0400

    feat:notifications (start)

--- foliage_scenes/src/notifications.rs
+++ foliage_scenes/src/notifications.rs
@@ -1,21 +1,27 @@
 use crate::Colors;
 use compact_str::{CompactString, ToCompactString};
 use foliage_macros::{inner_set_descriptor, InnerSceneBinding};
+use foliage_proper::animate::trigger::Trigger;
 use foliage_proper::bevy_ecs;
 use foliage_proper::bevy_ecs::bundle::Bundle;
 use foliage_proper::bevy_ecs::entity::Entity;
 use foliage_proper::bevy_ecs::prelude::{Component, IntoSystemConfigs};
 use foliage_proper::bevy_ecs::query::{Changed, With, Without};
 use foliage_proper::bevy_ecs::system::{Commands, Query, SystemParamItem};
+use foliage_proper::conditional::ConditionHandle;
 use foliage_proper::elm::config::{CoreSet, ElmConfiguration, ExternalSet};
 use foliage_proper::elm::leaf::{Leaf, Tag};
 use foliage_proper::elm::{Elm, Style};
 use foliage_proper::panel::Panel;
+use foliage_proper::procedure::Procedure;
 use foliage_proper::scene::micro_grid::{
     AlignmentDesc, AnchorDim, MicroGrid, MicroGridAlignment, RelativeMarker,
 };
 use foliage_proper::scene::{Binder, Bindings, Scene, SceneComponents, SceneHandle};
+use foliage_proper::segment::{ResponsiveSegment, Segment, SegmentUnitDesc};
 use foliage_proper::text::{MaxCharacters, Text, TextValue};
+use foliage_proper::view::ViewBuilder;
+
 #[derive(Clone, Component)]
 pub struct Notification(pub CompactString);
 impl Notification {
@@ -23,14 +29,28 @@ impl Notification {
         Self(s.as_ref().to_compact_string())
     }
 }
-pub struct Notifications {
+impl Procedure for NotificationBar {
+    fn steps(self, view_builder: &mut ViewBuilder) {
+        let e = view_builder.conditional_scene(
+            self,
+            ResponsiveSegment::base(Segment::new(
+                0.15.relative().to(0.7.relative()),
+                1.relative().offset(-64.0).to(56.absolute()),
+            )),
+        );
+        view_builder.extend(e.this(), NotificationState::Hidden);
+        view_builder.extend(e.this(), NotificationHandle(e));
+    }
+}
+#[derive(Clone)]
+pub struct NotificationBar {
     pub colors: Colors,
 }
 #[inner_set_descriptor]
 pub enum SetDescriptor {
     Update,
 }
-impl Leaf for Notifications {
+impl Leaf for NotificationBar {
     type SetDescriptor = SetDescriptor;
 
     fn config(_elm_configuration: &mut ElmConfiguration) {
@@ -39,7 +59,10 @@ impl Leaf for Notifications {
 
     fn attach(elm: &mut Elm) {
         elm.main().add_systems((
-            foliage_proper::scene::config::<Notifications>.in_set(SetDescriptor::Update),
+            foliage_proper::scene::config::<NotificationBar>
+                .in_set(SetDescriptor::Update)
+                .before(<Text as Leaf>::SetDescriptor::Update)
+                .before(<Panel as Leaf>::SetDescriptor::Update),
             engage_notification_bar.in_set(CoreSet::ProcessEvent),
         ));
     }
@@ -55,21 +78,20 @@ pub enum SnackBarBindings {
     LineOne,
     LineTwo,
 }
+#[derive(Component)]
+struct NotificationHandle(ConditionHandle);
 #[derive(Bundle)]
 pub struct NotificationsComponents {
-    pub state: NotificationState,
     pub notification: Notification,
 }
 fn engage_notification_bar(
-    query: Query<(Entity, &Notification), Without<Tag<Notifications>>>,
-    notification_listener: Query<
-        (&mut Notification, &mut NotificationState, &Bindings),
-        With<Tag<Notifications>>,
-    >,
+    query: Query<(Entity, &Notification), Without<Tag<NotificationBar>>>,
+    notification_listener: Query<(&mut Notification), With<Tag<NotificationBar>>>,
+    mut trigger_element: Query<(&mut NotificationState, &mut Trigger, &NotificationHandle)>,
     mut cmd: Commands,
 ) {
     for (entity, notification) in query.iter() {
-        // enable notification bar conditionals,
+        // enable notification bar conditional,
         // if !already_open,
         // -- anim from offscreen position_adjust
         // -- anim includes on-end 30s timer w/ on-end (timer) anim to hidden
@@ -78,7 +100,7 @@ fn engage_notification_bar(
         cmd.entity(entity).despawn();
     }
 }
-impl Scene for Notifications {
+impl Scene for NotificationBar {
     type Params = (Query<'static, 'static, &'static Notification>,);
     type Filter = Changed<Notification>;
     type Components = NotificationsComponents;
@@ -135,7 +157,6 @@ impl Scene for Notifications {
         binder.finish::<Self>(SceneComponents::new(
             MicroGrid::new(),
             NotificationsComponents {
-                state: NotificationState::Hidden,
                 notification: Notification::new(""),
             },
         ))
