0cc987aff88e72d1776f6fc94f86ab5f07cb21b4
eblack <eblack.leaf@proton.me>
2024-03-18 12:32:24 -0400

    feat:animate

--- foliage_proper/src/animate/mod.rs
+++ foliage_proper/src/animate/mod.rs
@@ -1,3 +1,31 @@
-pub mod trigger;
+use crate::time::{Time, TimeDelta};
+use bevy_ecs::prelude::{Component, Entity};
+use bevy_ecs::system::{Commands, Query, Res};
 
-pub trait Animate {}
+pub mod trigger;
+#[derive(Component, Copy, Clone)]
+pub struct AnimateTarget(pub Entity);
+pub struct InterpolationPercent(pub f32);
+pub struct InterpolationExtraction(pub f32);
+pub struct Interpolation();
+pub trait Interpolate {
+    fn interpolations() -> Vec<Interpolation>;
+    fn apply(&self, extracts: Vec<InterpolationExtraction>) -> Self;
+}
+pub trait Animate {
+    fn animate<I: Interpolate>(i: I) -> Animation<I>;
+}
+pub struct Animation<I: Interpolate> {}
+impl<I: Interpolate> Animation<I> {
+    pub fn to(mut self, i: I, duration: TimeDelta) -> Self {
+        // end + duration
+        self
+    }
+}
+fn apply<I: Interpolate>(
+    query: Query<(Entity, &mut Animation<I>)>,
+    mut targets: Query<&mut I>,
+    time: Res<Time>,
+    mut cmd: Commands,
+) {
+}
