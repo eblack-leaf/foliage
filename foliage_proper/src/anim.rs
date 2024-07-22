fb6501ef0ea26bed8901d4d90cf02e565ea03560
eblack-leaf <eblack.leaf@proton.me>
2024-07-22 14:50:29 -0400

    feat:anim-setup

--- foliage_proper/src/anim.rs
+++ foliage_proper/src/anim.rs
@@ -2,12 +2,15 @@ use crate::coordinate::section::Section;
 use crate::coordinate::LogicalContext;
 use crate::grid::GridPlacement;
 
+// when make => get current section of entity + set in new.queued_offset
+// read ending-offset from new.offset (default == 0/0/0/0)
+//
 pub(crate) struct PlacementAnimation {
     new: GridPlacement,
-    ending_offset: Section<LogicalContext>,
+    ending_offset: Section<LogicalContext>, // read from new grid-placement
 }
 pub struct Animation<A: Animate> {
-    current: Option<A>,
+    start: Option<A>,
     end: A,
 }
 pub trait Animate
