10e218500644c9f0ee031dac573b27efa6e54bbd
salt <eblack.leaf@proton.me>
2024-12-30 15:14:57 -0500

    feat:v0.6

--- foliage_proper/src/grid/location.rs
+++ foliage_proper/src/grid/location.rs
@@ -1,5 +1,5 @@
-use crate::grid::{AlignedUnit, ScalarUnit};
-use crate::{CoordinateUnit, Coordinates};
+use crate::grid::{AlignedUnit, GridUnit, ScalarUnit};
+use crate::Coordinates;
 
 pub struct Location {
     pub sm: Option<LocationConfiguration>,
@@ -7,17 +7,104 @@ pub struct Location {
     pub lg: Option<LocationConfiguration>,
     pub xl: Option<LocationConfiguration>,
 }
+impl Location {
+    pub fn new() -> Self {
+        Self {
+            sm: None,
+            md: None,
+            lg: None,
+            xl: None,
+        }
+    }
+    pub fn sm<HAD: Into<LocationAxisDescriptor>, VAD: Into<LocationAxisDescriptor>>(
+        mut self,
+        had: HAD,
+        vad: VAD,
+    ) -> Self {
+        self.sm.replace((had.into(), vad.into()).into());
+        self
+    }
+    pub fn md<HAD: Into<LocationAxisDescriptor>, VAD: Into<LocationAxisDescriptor>>(
+        mut self,
+        had: HAD,
+        vad: VAD,
+    ) -> Self {
+        self.md.replace((had.into(), vad.into()).into());
+        self
+    }
+    pub fn lg<HAD: Into<LocationAxisDescriptor>, VAD: Into<LocationAxisDescriptor>>(
+        mut self,
+        had: HAD,
+        vad: VAD,
+    ) -> Self {
+        self.lg.replace((had.into(), vad.into()).into());
+        self
+    }
+    pub fn xl<HAD: Into<LocationAxisDescriptor>, VAD: Into<LocationAxisDescriptor>>(
+        mut self,
+        had: HAD,
+        vad: VAD,
+    ) -> Self {
+        self.xl.replace((had.into(), vad.into()).into());
+        self
+    }
+}
 pub struct LocationConfiguration {
     pub horizontal: LocationAxisDescriptor,
     pub vertical: LocationAxisDescriptor,
+}
+impl From<(LocationAxisDescriptor, LocationAxisDescriptor)> for LocationConfiguration {
+    fn from(value: (LocationAxisDescriptor, LocationAxisDescriptor)) -> Self {
+        Self {
+            horizontal: value.0,
+            vertical: value.1,
+        }
+    }
+}
+pub struct Padding {
+    pub coordinates: Coordinates,
+}
+impl From<i32> for Padding {
+    fn from(value: i32) -> Self {
+        Self {
+            coordinates: Coordinates::from((value, value)),
+        }
+    }
+}
+impl From<(i32, i32)> for Padding {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            coordinates: Coordinates::from((value.0, value.1)),
+        }
+    }
 }
 pub struct LocationAxisDescriptor {
     pub a: LocationAxisUnit,
     pub b: LocationAxisUnit,
     pub ty: LocationAxisType,
-    pub padding: Coordinates,
+    pub padding: Padding,
     pub justify: Justify,
-    pub max: Option<CoordinateUnit>,
+    pub max: Option<ScalarUnit>,
+}
+impl LocationAxisDescriptor {
+    pub fn justify(mut self, justify: Justify) -> Self {
+        self.justify = justify;
+        self
+    }
+    pub fn pad<P: Into<Padding>>(mut self, pad: P) -> Self {
+        self.padding = pad.into();
+        self
+    }
+    pub fn max<S: Into<ScalarUnit>>(mut self, max: S) -> Self {
+        self.max.replace(max.into());
+        self
+    }
+}
+pub fn stack() -> GridUnit {
+    todo!()
+}
+pub fn auto() -> GridUnit {
+    todo!()
 }
 pub enum Justify {
     Left,
--- foliage_proper/src/grid/mod.rs
+++ foliage_proper/src/grid/mod.rs
@@ -1,11 +1,14 @@
+mod aspect_ratio;
 mod layout;
 mod location;
-mod aspect_ratio;
 
+use crate::grid::location::Justify::{Center, Left};
+use crate::grid::location::{auto, stack, LocationAxisDescriptor};
 use crate::{CoordinateUnit, Coordinates};
 pub use aspect_ratio::AspectRatio;
 pub use layout::Layout;
 pub use location::Location;
+
 #[test]
 fn behavior() {
     use crate::FontSize;
@@ -22,8 +25,11 @@ fn behavior() {
             2.col().to(11.col()).max(400.px()).justify(Center).pad(4),
             4.row().to(10.row()).pad((4, 8)), // debug-assert max only on width
         )
-        .md(3.col().to(10.col()).max(500.px()).justify(Center), 4.to(10));
-    let aspect_ratio = AspectRatio::new().sm(16 / 9);
+        .md(
+            3.col().to(10.col()).max(500.px()).justify(Center),
+            4.row().to(10.row()),
+        );
+    let aspect_ratio = AspectRatio::new().sm(16.0 / 9.0);
     let font_size = FontSize::default().md(20).lg(24).xl(32);
     let location = Location::new()
         .sm(
@@ -34,7 +40,7 @@ fn behavior() {
             4.col().to(9.col()).max(400.px()).justify(Left),
             6.row().to(9.row()),
         );
-    let location = Location::new().sm(1.col().to(1.col()), 2.to(auto()));
+    let location = Location::new().sm(1.col().to(1.col()), 2.row().to(auto()));
     let location = Location::new().sm(1.col().to(1.col()), stack().to(auto())); // stack uses stem().bottom() as this.top()
     let location = Location::new().sm(1.col().to(1.col()), stack().to(25.row())); // explicit back to grid (acceptable content-range) or keep stacking
     let location = Location::new().sm(1.col().to(1.col()), stack().span(5.row()));
@@ -42,18 +48,81 @@ fn behavior() {
     // span(5.row()) => 5 row lengths from current px() in stack (not necessarily aligned)
     // but spacing relative is guaranteed 5 rows for content
 }
+pub trait GridExt {
+    fn px(self) -> GridUnit;
+    fn pct(self) -> GridUnit;
+    fn col(self) -> GridUnit;
+    fn row(self) -> GridUnit;
+}
+impl GridExt for i32 {
+    fn px(self) -> GridUnit {
+        todo!()
+    }
+
+    fn pct(self) -> GridUnit {
+        todo!()
+    }
+
+    fn col(self) -> GridUnit {
+        todo!()
+    }
+
+    fn row(self) -> GridUnit {
+        todo!()
+    }
+}
 pub struct Grid {
     pub sm: GridConfiguration,
     pub md: Option<GridConfiguration>,
     pub lg: Option<GridConfiguration>,
     pub xl: Option<GridConfiguration>,
 }
+impl Grid {
+    pub fn new<HA: Into<GridAxisDescriptor>, VA: Into<GridAxisDescriptor>>(ha: HA, va: VA) -> Self {
+        Self {
+            sm: (ha.into(), va.into()).into(),
+            md: None,
+            lg: None,
+            xl: None,
+        }
+    }
+    pub fn md<HA: Into<GridAxisDescriptor>, VA: Into<GridAxisDescriptor>>(
+        mut self,
+        ha: HA,
+        va: VA,
+    ) -> Self {
+        self.md.replace((ha.into(), va.into()).into());
+        self
+    }
+    pub fn lg<HA: Into<GridAxisDescriptor>, VA: Into<GridAxisDescriptor>>(
+        mut self,
+        ha: HA,
+        va: VA,
+    ) -> Self {
+        self.lg.replace((ha.into(), va.into()).into());
+        self
+    }
+    pub fn xl<HA: Into<GridAxisDescriptor>, VA: Into<GridAxisDescriptor>>(
+        mut self,
+        ha: HA,
+        va: VA,
+    ) -> Self {
+        self.xl.replace((ha.into(), va.into()).into());
+        self
+    }
+}
 pub struct GridConfiguration {
     pub columns: GridAxisDescriptor,
+    pub rows: GridAxisDescriptor,
+}
+impl From<(GridAxisDescriptor, GridAxisDescriptor)> for GridConfiguration {
+    fn from((columns, rows): (GridAxisDescriptor, GridAxisDescriptor)) -> Self {
+        Self { columns, rows }
+    }
 }
 pub struct GridAxisDescriptor {
     pub unit: GridAxisUnit,
-    pub gap: Coordinates,
+    pub gap: Gap,
 }
 pub enum GridAxisUnit {
     Infinite(ScalarUnit),
@@ -62,6 +131,44 @@ pub enum GridAxisUnit {
 pub enum ScalarUnit {
     Px(CoordinateUnit),
     Pct(f32),
+}
+impl From<GridUnit> for ScalarUnit {
+    fn from(grid: GridUnit) -> Self {
+        match grid {
+            GridUnit::Aligned(_) => {
+                panic!("aligned cannot be scalar")
+            }
+            GridUnit::Scalar(s) => s,
+        }
+    }
+}
+pub struct Gap {
+    pub coordinates: Coordinates,
+}
+impl From<i32> for Gap {
+    fn from(x: i32) -> Self {
+        Gap {
+            coordinates: (x, x).into(),
+        }
+    }
+}
+pub enum GridUnit {
+    Aligned(AlignedUnit),
+    Scalar(ScalarUnit),
+}
+impl GridUnit {
+    pub fn gap<G: Into<Gap>>(self, g: G) -> GridAxisDescriptor {
+        todo!()
+    }
+    pub fn y<Y: Into<GridUnit>>(self, y: Y) -> LocationAxisDescriptor {
+        todo!()
+    }
+    pub fn to<T: Into<GridUnit>>(self, t: T) -> LocationAxisDescriptor {
+        todo!()
+    }
+    pub fn span<S: Into<GridUnit>>(self, s: S) -> LocationAxisDescriptor {
+        todo!()
+    }
 }
 pub enum AlignedUnit {
     Columns(u32),
