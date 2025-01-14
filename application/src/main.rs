6b9b1dff582be5f0f5058c35c3e31307bed2c7a3
salt <eblack.leaf@proton.me>
2025-01-13 23:11:52 -0500

    feat:v0.6

--- application/src/main.rs
+++ application/src/main.rs
@@ -74,6 +74,69 @@ fn main() {
         tree.animate(seq, Animation::new(Location::new().xs(1.col().to(4.col()), 7.row().to(12.row()))).start(0).finish(10000).targeting(n1));
         tree.sequence_end(seq, move |trigger: Trigger<OnEnd>, mut tree: Tree| {
             tree.remove(n1);
+            let nested = tree.leaf((
+                Grid::new(1.col().gap(0), 1.row().gap(0)),
+                Stem::some(root),
+                Elevation::new(-1),
+                Location::new().xs(1.col().to(2.col()), 1.row().to(6.row())),
+                InteractionListener::new().scroll(true),
+            ));
+            let element = tree.leaf((
+                Text::new("abc def ghi jkl mnopqr stuvwx yzABC DEF GHIKJLM NOPQRSTU VWXY abc def ghi jkl mnopqr stuvwx yzABC DEF GHIKJLM NOPQRSTU VWXY abc def ghi jkl mnopqr stuvwx yzABC DEF GHIKJLM NOPQRSTU VWXY"),
+                FontSize::new(24),
+                Location::new().xs(0.pct().to(100.pct()), 1.row().span(auto())),
+                Grid::default(),
+                AutoHeight(true),
+                Elevation::new(-2),
+                Stem::some(nested),
+            ));
+            let drag_test = tree.leaf((
+                Panel::new(),
+                Color::gray(250),
+                Elevation::new(-2),
+                Location::new().xs(0.pct().to(100.pct()), stack().span(100.px())),
+                Stack::new(element),
+                Stem::some(nested),
+                InteractionListener::new(),
+            ));
+            let supr_nest = tree.leaf((
+                Panel::new(),
+                Color::gray(350),
+                Location::new().xs(0.pct().to(100.pct()), stack().span(200.px())),
+                Elevation::new(-3),
+                Stem::some(nested),
+                Stack::new(drag_test),
+                Grid::default(),
+                InteractionListener::new().scroll(true),
+            ));
+            let supr_nest_text = tree.leaf((
+                Text::new(" osaeta oeu u uu u u u u u  u u u u  u u  uu  uu u u u  u uu u u  u u u u u  uuuuuuuuu u uuuuu uuuuuuuu uuuuu uuuuuu uuu u u u u u uu u u uuu u uuu u u u uuuuuuuu uuuuuuuuu"),
+                FontSize::new(24),
+                Elevation::new(-2),
+                Stem::some(supr_nest),
+                AutoHeight(true),
+                Location::new().xs(0.pct().to(100.pct()), 10.px().span(auto())),
+            ));
+            tree.entity(drag_test).observe(move |trigger: Trigger<OnClick>, mut tree: Tree| {
+                let seq = tree.sequence();
+                tree.animate(seq, Animation::new(Location::new().xs(1.col().to(4.col()), 1.row().to(6.row()))).start(0).finish(10000).targeting(nested));
+                tree.sequence_end(seq, move |trigger: Trigger<OnEnd>, mut tree: Tree| {
+                    tree.remove(nested);
+                    println!("did");
+                });
+                println!("did-it --------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            });
+            let nested_backdrop = tree.leaf((
+                Panel::new(),
+                Color::gray(500),
+                Elevation::new(1),
+                Location::new().xs(0.pct().to(100.pct()), 0.pct().to(1000.px())),
+                Stem::some(element),
+            ));
+            println!(
+                "r: {:?} rb: {:?} n: {:?} e: {:?} dt: {:?} sn: {:?} snt: {:?} nb: {:?}",
+                root, root_backdrop, nested, element, drag_test, supr_nest, supr_nest_text, nested_backdrop
+            );
             println!("done");
         });
         println!("done-it --------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
@@ -88,70 +151,6 @@ fn main() {
     println!(
         "r: {:?} rb: {:?} n: {:?} e: {:?} dt: {:?} sn: {:?} snt: {:?} nb: {:?}",
         root, root_backdrop, n1, e1, dt1, sn1, snt1, nb1
-    );
-
-    let nested = foliage.leaf((
-        Grid::new(1.col().gap(0), 1.row().gap(0)),
-        Stem::some(root),
-        Elevation::new(-1),
-        Location::new().xs(1.col().to(2.col()), 1.row().to(6.row())),
-        InteractionListener::new().scroll(true),
-    ));
-    let element = foliage.leaf((
-        Text::new("abc def ghi jkl mnopqr stuvwx yzABC DEF GHIKJLM NOPQRSTU VWXY abc def ghi jkl mnopqr stuvwx yzABC DEF GHIKJLM NOPQRSTU VWXY abc def ghi jkl mnopqr stuvwx yzABC DEF GHIKJLM NOPQRSTU VWXY"),
-        FontSize::new(24),
-        Location::new().xs(0.pct().to(100.pct()), 1.row().span(auto())),
-        Grid::default(),
-        AutoHeight(true),
-        Elevation::new(-2),
-        Stem::some(nested),
-    ));
-    let drag_test = foliage.leaf((
-        Panel::new(),
-        Color::gray(250),
-        Elevation::new(-2),
-        Location::new().xs(0.pct().to(100.pct()), stack().span(100.px())),
-        Stack::new(element),
-        Stem::some(nested),
-        InteractionListener::new(),
-    ));
-    let supr_nest = foliage.leaf((
-        Panel::new(),
-        Color::gray(350),
-        Location::new().xs(0.pct().to(100.pct()), stack().span(200.px())),
-        Elevation::new(-3),
-        Stem::some(nested),
-        Stack::new(drag_test),
-        Grid::default(),
-        InteractionListener::new().scroll(true),
-    ));
-    let supr_nest_text = foliage.leaf((
-        Text::new(" osaeta oeu u uu u u u u u  u u u u  u u  uu  uu u u u  u uu u u  u u u u u  uuuuuuuuu u uuuuu uuuuuuuu uuuuu uuuuuu uuu u u u u u uu u u uuu u uuu u u u uuuuuuuu uuuuuuuuu"),
-        FontSize::new(24),
-        Elevation::new(-2),
-        Stem::some(supr_nest),
-        AutoHeight(true),
-        Location::new().xs(0.pct().to(100.pct()), 10.px().span(auto())),
-    ));
-    foliage.world.commands().entity(drag_test).observe(move |trigger: Trigger<OnClick>, mut tree: Tree| {
-        let seq = tree.sequence();
-        tree.animate(seq, Animation::new(Location::new().xs(1.col().to(4.col()), 1.row().to(6.row()))).start(0).finish(10000).targeting(nested));
-        tree.sequence_end(seq, move |trigger: Trigger<OnEnd>, mut tree: Tree| {
-            tree.remove(nested);
-            println!("did");
-        });
-        println!("did-it --------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-    });
-    let nested_backdrop = foliage.leaf((
-        Panel::new(),
-        Color::gray(500),
-        Elevation::new(1),
-        Location::new().xs(0.pct().to(100.pct()), 0.pct().to(1000.px())),
-        Stem::some(element),
-    ));
-    println!(
-        "r: {:?} rb: {:?} n: {:?} e: {:?} dt: {:?} sn: {:?} snt: {:?} nb: {:?}",
-        root, root_backdrop, nested, element, drag_test, supr_nest, supr_nest_text, nested_backdrop
     );
     foliage.photosynthesize(); // run
 }
